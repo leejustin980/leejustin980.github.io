2cb9bb817ac53468414c2a76725bb0e54508f44b
Justin <leejustin980@gmail.com>
2018-10-05 17:35:03 -0400

    New Website Design
    Updated Design, Resume, and Links

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+
+function preload() {
+
+
+
+}
+
+
+
+function setup() {
+	// canvas - fit to window
+	//createCanvas(windowWidth, windowHeight);
+	console.log("hi");
+}
+
+
+function draw() {
+
+
+}
